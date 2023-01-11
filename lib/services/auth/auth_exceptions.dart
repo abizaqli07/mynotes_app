337a7d1625a57ece4d9a600b80f408def5ef75a6
abikhoir07 <abikhoir42@gmail.com>
2023-01-11 11:04:00 +0700

    create auth services

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,14 @@
+
+
+// Login exeptions
+class UserNotFoundAuthException implements Exception {}
+class WrongPasswordAuthException implements Exception {}
+
+// Register Exceptions
+class WeakPasswordAuthException implements Exception {}
+class EmailAlreadyInUseAuthException implements Exception {}
+class InvalidEmailAuthException implements Exception {}
+
+// Generic Exceptions
+class GenericAuthException implements Exception {}
+class UserNotLoggedInAuthException implements Exception {}
